edf66e92ad0ea17cba67ddff6999153ab47c5ea3
asaveliev <saveliev@gmail.com>
2015-07-02 09:03:39 -0700

    upgraded to new version: new directive properties: player status, duration, quality, start time logging capabilities for debugging (uncomment to use) fixed scrolling fixed time display to match what youtube does implemented video length detection without use of gdata

--- ngtube.js
+++ ngtube.js
@@ -1,4 +1,6 @@
+var playerAPIloaded = false;
 function onYouTubeIframeAPIReady(event) {
+	playerAPIloaded = true;
 	angular.element(document.querySelector('body')).scope().$broadcast("playerAPIloaded");
 }
 
@@ -7,7 +9,11 @@ angular.module('ngTube',[])
 	return {
 		restrict: "E",
 		scope : {
-			mediaUrl : "@"
+			url : "@",
+			playerStatus : "=status",
+			duration : "@",
+			quality : "@",
+			startseconds: "@"
 		},
 		link : function ($scope, element, attrs){
 
@@ -21,10 +27,24 @@ angular.module('ngTube',[])
 			$scope.dragging = false;
 			var lastDraggedPosition = 0;
 
+
+			function getOffsetSum(elem) {
+				var top=0, left=0;
+
+				while(elem) {
+					top = top + parseInt(elem.offsetTop);
+					left = left + parseInt(elem.offsetLeft);
+					elem = elem.offsetParent;
+				}
+
+				return {top: top, left: left}
+			}
+
+
 			element[0].querySelector(".timeline").addEventListener(
 	            'click',
 	            function(e) {
-					$scope.scrollTo(e.offsetX);
+					$scope.scrollToCoord(e.offsetX);
 	            },
 	            false
 	        );
@@ -32,6 +52,9 @@ angular.module('ngTube',[])
 			var el = element[0].querySelector(".currentposition");
 			var interceptor = element[0].querySelector(".ngtubedraginterceptor");
 			var timeline = element[0].querySelector(".timeline");
+			var timelineOffset = getOffsetSum(timeline);
+			$scope.leftFullOffset = timelineOffset.left;
+			$scope.leftOffset = timeline.offsetLeft;
 
 	        $scope.cursorSize = el.offsetWidth;
 			$scope.scrollWidth = timeline.offsetWidth;
@@ -59,7 +82,7 @@ angular.module('ngTube',[])
 			document.addEventListener(
 	            'mousemove',
 	            function(e) {
-	            	var pos = e.x - timeline.offsetLeft;
+	            	var pos = e.x - $scope.leftFullOffset;
 	            	if ($scope.dragging && pos < $scope.scrollWidth && pos >=0 ) {
 	                	$scope.setPosition(pos);
 	                	$scope.setCurrentTime(pos / $scope.scrollRatio);
@@ -75,7 +98,7 @@ angular.module('ngTube',[])
 	            function(e) {
 	            	if ($scope.dragging) { 
 		                $scope.dragging = false;
-		            	$scope.scrollTo(lastDraggedPosition);
+		            	$scope.scrollToCoord(lastDraggedPosition);
 		                interceptor.style.pointerEvents = "none";
 		                document.body.style.userSelect = "auto";
 	            	}
@@ -91,56 +114,58 @@ angular.module('ngTube',[])
 	    	else
 		    return ""+
 			"<div class='ngtubeplayer'>"+
-				"bar<div class='ngtubeplayercontainer'> "+
+				"<div class='ngtubeplayercontainer'> "+
 					"<div class='ngtubedraginterceptor'></div>"+
 			    	"<div id='youtubeplayer'></div> "+
 				"</div>"+
 			    "<div class='ngtubetimeline'> "+
 			        "<div class='timeline'></div> "+
-			        "<div class='currentposition' ng-style=\"{'left':leftOffset}\"></div> "+
+			        "<div class='currentposition' ng-style=\"{'left':cursorLeftOffset+'px'}\"></div> "+
 			    "</div> "+
 			    "<div class='ngtubecontrols'>"+
 				    "<button ng-click='goBack()'>&lt; 10s</button> "+
 				    "<button ng-click='playPause()''>"+
 				    	"<span ng-show='playerState != 1'>&gt;</span><span ng-show='playerState == 1'>||</span></button> "+
 				    "<button ng-click='mute()'>"+
-				    	"<span ng-show='isMuted'>Mute</span><span ng-show='!isMuted'>Unmute</span></button> "+
+				    	"<span ng-show='isMuted'>Unmute</span><span ng-show='!isMuted'>Mute</span></button> "+
 				    "<input class='ngtubevolume' type='range' min='0' max='100' ng-model='volume'></input>" +
 				    "<span class='ngtubetimer'>{{currentTimeStr}} / {{durationStr}}</span>" +
 			    "</div>" +
 			"</div> "
 		},
 	    controller : ["$scope","$interval",function($scope,$interval){
-	        $scope.$on("playerAPIloaded", function () {
+
+	    	var playerSetup = false;
+
+	    	function createPlayer(){
+				log("ngTube - createPlayer");
 				$scope.ytplayer = new YT.Player('youtubeplayer', {
 					playerVars: { 'autoplay': 0, 'controls': 0, 'iv_load_policy': 3, 'playsinline': 1, 
 						'rel': 0, 'showinfo': 0, 'theme': 'light', wmode: 'opaque' },
 					events: {
 						'onReady': function () {
-							
+							log("ngTube - createPlayer created");
+							playerSetup = true;
 							$scope.cueVideo();
-
-							$scope.ytplayer.mute();
-							$scope.volume = $scope.ytplayer.getVolume();
-
-							$scope.ytplayer.playVideo();
-							var timeDetector = $interval(function(){
-								if ($scope.ytplayer.getDuration() > 0) {
-									$scope.duration = $scope.ytplayer.getDuration();
-									$scope.durationStr = formatSeconds($scope.duration,$scope.duration);
-									$scope.ytplayer.pauseVideo();
-								    $scope.ytplayer.unMute();
-
-									$scope.scrollRatio  = $scope.scrollWidth / $scope.duration;
-									$scope.ytplayer.seekTo(0);
-									$interval.cancel(timeDetector);
-								}
-							},20);
-
 						}
 					}
 				});
-			});
+	    	}
+
+	    	$scope.hidePlayButton = false;
+	    	if (navigator.userAgent.indexOf("Mobile") > 0 || navigator.userAgent.indexOf("Android") > 0)
+	    		$scope.hidePlayButton = true;
+
+	    	function log(text){
+	    		//console.log(text);
+	    	}
+
+	    	if (!playerAPIloaded)
+		        $scope.$on("playerAPIloaded", function () {
+		        	createPlayer();
+				});
+		    else
+		    	createPlayer();
 
 			$scope.playerState = 0;
 			$scope.isMuted = false;
@@ -165,59 +190,128 @@ angular.module('ngTube',[])
 				var t_minutes = Math.floor((t_sec_num - (t_hours * 3600)) / 60);
 				var t_seconds = t_sec_num - (t_hours * 3600) - (t_minutes * 60);
 
-				var time    = (t_hours > 0 ? pad(hours,2)+':':"")+(t_minutes > 0 ? pad(minutes,2)+':':"")+pad(seconds,2);
+				var time    = (t_hours > 0 ? pad(hours,2)+':':"")+ pad(minutes,2)+':'+pad(seconds,2);
 				return time;
 			}
 
 			$scope.cueVideo = function(){
-				$scope.ytplayer.cueVideoByUrl({ 
-					mediaContentUrl: $scope.mediaUrl 
-				});
+
+				if (playerSetup) {
+					log("ngTube - cueVideo " + $scope.url);
+
+					$scope.ytplayer.cueVideoByUrl({ 
+						mediaContentUrl: $scope.url,
+						startSeconds: $scope.startseconds,
+						suggestedQuality: ($scope.quality ? $scope.quality : "default")
+					});
+
+					$scope.volume = $scope.ytplayer.getVolume();
+
+					if ($scope.duration) {
+						$scope.durationStr = formatSeconds($scope.duration,$scope.duration);
+						$scope.scrollRatio  = $scope.scrollWidth / $scope.duration;
+						return;
+					}
+
+					log("ngTube - cueVideo determining duration");
+					$scope.ytplayer.mute();
+					$scope.ytplayer.playVideo();
+					var timeDetector = $interval(function(){
+						if ($scope.ytplayer.getDuration() > 0) {
+							$scope.duration = $scope.ytplayer.getDuration();
+							$scope.durationStr = formatSeconds($scope.duration,$scope.duration);
+							$scope.ytplayer.pauseVideo();
+						    $scope.ytplayer.unMute();
+
+							$scope.scrollRatio  = $scope.scrollWidth / $scope.duration;
+							$scope.ytplayer.seekTo(0);
+							$interval.cancel(timeDetector);
+						}
+					},20);
+				}
 			}
 
+			$scope.$on("scrollPlayer",function(event,time){
+				log("ngTube - got event to scroll to "+time);
+				$scope.scrollToTime(time);
+			});
+
+			$scope.$on("pausePlayer",function(event,time){
+        		log("ngTube - pause");
+	            $scope.ytplayer.pauseVideo();
+			});
+
+			$scope.$watch("url",function(url){
+        		log("ngTube - watch " + url);
+				$scope.cueVideo();
+			});
+
 			$scope.$watch("volume",function(volume){
-				if ($scope.ytplayer)
+				if (playerSetup)
 					$scope.ytplayer.setVolume(volume);
 			});
 
 			var scroller = $interval(function () {
-				if ($scope.ytplayer && $scope.ytplayer.getCurrentTime) {
-					$scope.currentTime = $scope.ytplayer.getCurrentTime();
+				if (playerSetup) {
+
+		        	log("ngTube - state " + $scope.ytplayer.getPlayerState());
+
+					if (!$scope.currentTime || $scope.ytplayer.getCurrentTime() > 0) 
+						$scope.currentTime = $scope.ytplayer.getCurrentTime();
+
+					if ($scope.currentTime > 0) $scope.hidePlayButton = false;
+
+					$scope.playerStatus = {"currentTime":$scope.currentTime};
 
 					$scope.playerState = $scope.ytplayer.getPlayerState();
 					$scope.isMuted = $scope.ytplayer.isMuted();
 
 					if (!$scope.dragging){
+						//log("ngTube - scroller at " + $scope.currentTime);
 		            	$scope.setPosition($scope.currentTime * $scope.scrollRatio);
 						$scope.setCurrentTime($scope.currentTime);
 					}
+
+					$scope.$emit("playerStatusChange");
 		        }
 			},100);
 
 			$scope.setCurrentTime = function(time){
 				$scope.currentTime = time;
+				$scope.playerStatus = {"currentTime":$scope.currentTime};
 				$scope.currentTimeStr = formatSeconds(time, $scope.duration);
+				//log("ngTube setCurrentTime(" + $scope.playerStatus.currentTime+")");
 			}
 
 	        $scope.$on("$destroy", function () {
 	          if (angular.isDefined(scroller)) {
-	            interval.cancel(scroller);
+	            $interval.cancel(scroller);
 	            scroller = undefined;
 	          }
 	        });
 
 	        $scope.setPosition = function (x) {
-	          $scope.leftOffset = (x - $scope.cursorSize / 2) + "px";
+	          $scope.cursorLeftOffset = x;
 	        }
 
 	        $scope.playPause = function () {
-	          if ($scope.ytplayer.getPlayerState() == 1)
+	        	if ($scope.ytplayer.getPlayerState() == 1){
+	        		log("ngTube - pause");
+	            	$scope.ytplayer.pauseVideo();
+        		}
+	        	else {
+	        		log("ngTube - play");
+	            	$scope.ytplayer.playVideo();
+	        	}
+	        }
+
+	        $scope.pauseVideo = function(){
+        		log("ngTube - pause");
 	            $scope.ytplayer.pauseVideo();
-	          else
-	            $scope.ytplayer.playVideo();
 	        }
 
 	        $scope.goBack = function () {
+	          log("ngTube - go back 10 seconds");
 	          $scope.ytplayer.seekTo($scope.currentTime - 10);
 	        }
 
@@ -228,10 +322,19 @@ angular.module('ngTube',[])
 	        		$scope.ytplayer.mute();
 	        }
 
-	        $scope.scrollTo = function (x) {
-	          var position = x / $scope.scrollRatio;
-	          $scope.ytplayer.seekTo(position);
+	        $scope.scrollToCoord = function (x) {
+	          log("ngTube - scrollToCoord(" + x + ")");
+	          var time = x / $scope.scrollRatio;
+	          $scope.setCurrentTime(time);
+	          $scope.ytplayer.seekTo(time);
 	          $scope.setPosition(x);
+	        }
+
+	        $scope.scrollToTime = function (time) {
+		        log("ngTube - scrollToTime(" + time+")");
+				$scope.ytplayer.seekTo(time);
+				$scope.setCurrentTime(time);
+				$scope.setPosition(time * $scope.scrollRatio);
 	        }
 
 	    }]
